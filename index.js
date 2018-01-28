d66dab3783e5aea02c206901e8f370d5cc5928c3
prewittdavon <davonprewitt@lawn-128-61-61-76.lawn.gatech.edu>
2018-01-28 00:23:41 -0500

    Created tutorial version

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var width = 960,
-    height = 500;
-
-d3.json("data.json", function(data) {
-	maxVal = data[0].frequency
-	d3.select(".chart")
-	    .attr("width", width)
-    	.attr("height", height)
-		.selectAll("div")
-		.data(data) //data being sent in
-			.enter() //invokes new data points
-			.append("div")
-			.style("height", 30)
-			.style("width", function(data) {return (data.frequency/maxVal*1.0)*960 + "px"})
-			.text(function(data) {return data.word})
-
-});
+// Intentionally left blank
